f761fe270aaddb9af1e533c6709094991f89962b
Remy Windsor <99709675+remywindsor@users.noreply.github.com>
2023-02-10 15:34:22 -0800

    Enable signout in Settings tab

--- screens/Settings.tsx
+++ screens/Settings.tsx
@@ -1,5 +1,28 @@
-import { View } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import { auth } from "../firebaseConfig";
 
 export default function SettingsScreen() {
-  return <View></View>;
+  return (
+    <View>
+      <Pressable onPress={() => auth.signOut().catch()} style={styles.setting}>
+        <Ionicons name="exit" size={18} />
+        <Text style={styles.settingText}>Sign out</Text>
+      </Pressable>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  setting: {
+    alignItems: "center",
+    backgroundColor: "#fff",
+    flexDirection: "row",
+    height: 64,
+    paddingHorizontal: 16,
+  },
+  settingText: {
+    fontSize: 18,
+    marginLeft: 8,
+  },
+});
