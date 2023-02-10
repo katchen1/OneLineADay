f5bb596047281987aa09eaaec1b1f910fef5a86e
Remy Windsor <99709675+remywindsor@users.noreply.github.com>
2023-02-10 11:40:37 -0800

    Set up Firebase SDK

---
+++ firebaseConfig.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCySvJMa7LFCzTjTBVtsdUOfLoajr-hTdc",
+  appId: "1:989263156206:web:c557d0dfb04a00da40f4ff",
+  authDomain: "cs-194-team-27.firebaseapp.com",
+  databaseURL: "https://cs-194-team-27.firebaseio.com",
+  projectId: "cs-194-team-27",
+  storageBucket: "cs-194-team-27.appspot.com",
+};
+
+const app = initializeApp(firebaseConfig);
