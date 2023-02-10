74336807e47aaed8f4b414970104c86ca5d1dcfe
Remy Windsor <99709675+remywindsor@users.noreply.github.com>
2023-02-10 12:38:10 -0800

    Enable bottom tab navigation

--- App.tsx
+++ App.tsx
@@ -1,20 +1,38 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import AnalyticsScreen from "./screens/Analytics";
+import FriendActivityScreen from "./screens/FriendActivity";
+import HomeScreen from "./screens/Home";
+import SettingsScreen from "./screens/Settings";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="Home"
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            switch (route.name) {
+              case "Home":
+                return <Ionicons name="home" size={size} color={color} />;
+              case "Analytics":
+                return <Ionicons name="analytics" size={size} color={color} />;
+              case "Friend Activity":
+                return <Ionicons name="people" size={size} color={color} />;
+              case "Settings":
+                return <Ionicons name="settings" size={size} color={color} />;
+            }
+          },
+        })}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Analytics" component={AnalyticsScreen} />
+        <Tab.Screen name="Friend Activity" component={FriendActivityScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
