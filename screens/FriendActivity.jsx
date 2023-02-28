4da0db7c312aa350e4ed039189330d49362043e1
qihan96 <qihan96@stanford.edu>
2023-02-28 01:01:31 -0800

    Set up the social tab's opt-in page

--- screens/FriendActivity.jsx
+++ screens/FriendActivity.jsx
@@ -1,5 +1,65 @@
-import { View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
-export default function FriendActivityScreen() {
-  return <View></View>;
-}
+//export default function FriendActivityScreen() {
+//return <View></View>;
+//}
+
+const SocialTab = () => {
+  const handleOptIn = () => {
+    console.log("User clicked the opt-in button");
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.tab_description}>
+        Connect with your friends and interact with each others' journals.
+      </Text>
+      <Text style={styles.tab_description}>
+        Opt in to the social mode at any time. You will have control over who
+        has access to your journal entries.
+      </Text>
+      <Pressable
+        style={({ pressed }) => [
+          { backgroundColor: pressed ? "grey" : "#305dbf" },
+          styles.optin_button,
+        ]}
+        onPress={handleOptIn}
+      >
+        <Text style={styles.optin_text}> Opt In</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  optin_button: {
+    //backgroundColor: "#305dbf",
+    borderRadius: 10,
+    height: 60,
+    width: 150,
+    marginHorizontal: 10,
+    marginVertical: 5,
+    padding: 10,
+    justifyContent: "center",
+  },
+  optin_text: {
+    fontSize: 28,
+    fontWeight: "500",
+    color: "white",
+    textAlign: "center",
+  },
+  tab_description: {
+    fontSize: 18,
+    color: "263238",
+    paddingRight: 50,
+    paddingLeft: 50,
+    paddingBottom: 15,
+  },
+});
+
+export default SocialTab;
