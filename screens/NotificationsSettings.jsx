b7c2a6a7b8d4329481799617354adba8332fb9c2
Remy Windsor <99709675+remywindsor@users.noreply.github.com>
2023-03-05 23:42:52 -0800

    Allow user to set journal reminder time

--- screens/NotificationsSettings.jsx
+++ screens/NotificationsSettings.jsx
@@ -1,11 +1,15 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import DateTimePicker from "@react-native-community/datetimepicker";
 import { doc, getDoc, updateDoc } from "firebase/firestore";
 import { useEffect, useState } from "react";
-import { Switch, Text, View } from "react-native";
+import { Pressable, Switch, Text, View } from "react-native";
 import { auth, db } from "../firebaseConfig";
 
 export default function NotificationsSettingsScreen() {
   const [isLoading, setIsLoading] = useState(true);
   const [notificationsEnabled, setNotificationsEnabled] = useState(false);
+  const [notificationTime, setNotificationTime] = useState([12, 0]);
+  const [showTimePicker, setShowTimePicker] = useState(false);
 
   useEffect(() => {
     (async () => {
@@ -24,10 +28,22 @@ export default function NotificationsSettingsScreen() {
             setNotificationsEnabled(false);
             break;
         }
-        setIsLoading(false);
       } catch (error) {
         console.error(error);
+        return;
       }
+
+      const storedNotificationTime = await AsyncStorage.getItem(
+        "notificationTime"
+      );
+      if (storedNotificationTime === null) {
+        await AsyncStorage.setItem("notificationTime", JSON.stringify([12, 0]));
+        setNotificationTime([12, 0]);
+      } else {
+        setNotificationTime(JSON.parse(storedNotificationTime));
+      }
+
+      setIsLoading(false);
     })();
   }, []);
 
@@ -39,6 +55,15 @@ export default function NotificationsSettingsScreen() {
     } catch (error) {
       console.error(error);
     }
+  }
+
+  async function handleTimePickerChange(_event, selectedTime) {
+    await AsyncStorage.setItem(
+      "notificationTime",
+      JSON.stringify([selectedTime.getHours(), selectedTime.getMinutes()])
+    );
+    setShowTimePicker(false);
+    setNotificationTime([selectedTime.getHours(), selectedTime.getMinutes()]);
   }
 
   if (isLoading) {
@@ -54,6 +79,22 @@ export default function NotificationsSettingsScreen() {
           onValueChange={toggleNotifications}
         />
       </View>
+      <Pressable
+        className="flex h-16 flex-row items-center justify-between bg-white px-4"
+        onPress={() => setShowTimePicker(true)}
+      >
+        <Text className="text-lg">Journal reminder time</Text>
+        <Text className="text-lg text-gray-500">{`${
+          notificationTime[0]
+        }:${notificationTime[1].toString().padStart(2, "0")}`}</Text>
+      </Pressable>
+      {showTimePicker ? (
+        <DateTimePicker
+          mode="time"
+          value={new Date(2000, 0, 1, notificationTime[0], notificationTime[1])}
+          onChange={handleTimePickerChange}
+        />
+      ) : null}
     </>
   );
 }
