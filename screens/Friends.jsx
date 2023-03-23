57026684076f72e9f7c4d5eccfa70912da6479f5
katchen1 <katherinechen139@gmail.com>
2023-03-22 21:58:42 -0700

    User can search for friends and add/remove friends

--- screens/Friends.jsx
+++ screens/Friends.jsx
@@ -1,9 +1,10 @@
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { useNavigation } from "@react-navigation/native";
-import { collection, documentId, getDocs, query, where } from "firebase/firestore";
+import { collection, doc, getDoc, getDocs, query, setDoc } from "firebase/firestore";
 import React from "react";
 import { FlatList, StyleSheet, Text, View } from "react-native";
-import { db } from "../firebaseConfig";
+import Autocomplete from "react-native-autocomplete-input";
+import { auth, db } from "../firebaseConfig";
 
 class FriendsScreen extends React.Component {
   constructor(props) {
@@ -13,46 +14,125 @@ class FriendsScreen extends React.Component {
       friendUids: props.route.params.data, // list of friend uids
       friends: [], // each friend has uid, name, email
       isLoading : true,
+      query: "",
+      filteredFriends: [],
+      allUsers: [],
     };
   }
 
   // Invoked immediately after the component is mounted  
   async componentDidMount() {
     this.navigation.setOptions({ title: "Friends"});
+    this.userRef = doc(db, "users", auth.currentUser.uid);
     this.queryFriends();
   }
 
   // Query friends by list of uids
   queryFriends = async() => {
-    const q = query(collection(db, "users"), where(documentId(), "in", this.state.friendUids));
+    let friends = [];
+    let allUsers = [];
+    const q = query(collection(db, "users"));
     const querySnapshot = await getDocs(q);
     querySnapshot.forEach((doc) => {
-      let friendUid = doc.id;
-      let friendName = doc.data().name;
-      let friendEmail = doc.data().email;
-      this.setState({friends: this.state.friends.concat({uid: friendUid, name: friendName, email: friendEmail})});
+      let uid = doc.id;
+      let name = doc.data().name;
+      let email = doc.data().email;
+      if (uid != auth.currentUser.uid) {
+        allUsers.push({uid: uid, name: name, email: email});
+      }
+      if (this.state.friendUids.includes(uid)) {
+        friends.push({uid: uid, name: name, email: email});
+      }
     });
-    this.setState({isLoading: false});
+    this.setState({friends: friends, allUsers: allUsers, isLoading: false});
   }
 
   // When the remove friend button is pressed
-  removeFriendOnPress = () => {
-    console.log("remove friend on press");
+  removeFriendOnPress = (user) => {
+    let friendsNew = this.state.friends.slice();
+    let index = -1;
+    for (let i = 0; i < friendsNew.length; i++) {
+      if (friendsNew[i].uid == user.uid) {
+        index = i;
+        break;
+      }
+    }
+    friendsNew.splice(index, 1);
+    this.setState({friends: friendsNew});
+    this.removeFriendFirestore(user.uid);
+  }
+
+  removeFriendFirestore = async(friendUid) => {
+    // Remove friend from user's list
+    let friendUidsNew = this.state.friendUids.slice();
+    let index = friendUidsNew.indexOf(friendUid);
+    friendUidsNew.splice(index, 1);
+    this.setState({friendUids: friendUidsNew});
+    setDoc(this.userRef, { friends: friendUidsNew }, { merge: true }); 
+
+    // Remove user from friend's list
+    let friendRef = doc(db, "users", friendUid);
+    let docSnap = await getDoc(friendRef);
+    if (docSnap.exists) {
+      friendUidsNew = docSnap.data().friends;
+      index = friendUidsNew.indexOf(auth.currentUser.uid);
+      friendUidsNew.splice(index, 1);
+      setDoc(friendRef, { friends: friendUidsNew }, { merge: true });
+    }
+  }
+
+  // When the add friend button is pressed
+  addFriendOnPress = (user) => {
+    this.setState({friends: this.state.friends.concat(user)});
+    this.addFriendFirestore(user.uid);
+  }
+
+  addFriendFirestore = async(friendUid) => {
+    // Add friend to user's list
+    let friendUidsNew = this.state.friendUids.concat(friendUid);
+    this.setState({friendUids: friendUidsNew});
+    setDoc(this.userRef, { friends: friendUidsNew }, { merge: true });
+
+    // Add user to friend's list
+    // Remove user from friend's list
+    let friendRef = doc(db, "users", friendUid);
+    let docSnap = await getDoc(friendRef);
+    if (docSnap.exists) {
+      friendUidsNew = docSnap.data().friends.concat(auth.currentUser.uid);
+      setDoc(friendRef, { friends: friendUidsNew }, { merge: true });
+    } 
   }
 
   // Render a friend item
   ItemRender = ({ friend }) => {
     return <View style={styles.item}>
-      <View style>
+      <View>
         <Text style={styles.friendNameText}>{friend.name}</Text>
         <Text style={styles.friendEmailText}>{friend.email}</Text>
       </View>
-      <Ionicons style={styles.removeButton} name="person-remove" size={28} onPress={this.removeFriendOnPress} />
+      <Ionicons style={styles.removeButton} name="person-remove" size={28} onPress={() => this.removeFriendOnPress(friend)} />
     </View>
   }
 
   ItemDivider = () => {
     return <View style={styles.divider} />
+  }
+
+  friendQueryOnChange = (text) => {
+    this.setState({ query: text });
+    if (text) {
+      // Making a case insensitive regular expression
+      const regex = new RegExp(`${text.trim()}`, 'i');
+      // Setting the filtered friends array according to the query
+      this.setState({filteredFriends: 
+        this.state.allUsers.filter(
+          (user) => user.email.search(regex) >= 0 || user.name.search(regex) >= 0
+        )
+      });
+    } else {
+      // If the query is null then return blank
+      this.setState({filteredFriends: []});
+    }
   }
   
   render() {
@@ -60,7 +140,30 @@ class FriendsScreen extends React.Component {
     if (this.state.isLoading) {
       return <Text>Loading...</Text>
     }
-    return <View>
+
+    return <View style={styles.container}>
+      <Autocomplete inputContainerStyle={styles.autocomplete}
+        data={this.state.filteredFriends}
+        value={this.state.query}
+        placeholder="Search friends..."
+        onChangeText={(text) => this.friendQueryOnChange(text)}
+        flatListProps={{
+          keyExtractor: (_, idx) => idx,
+          renderItem: ({ item }) => <View style={styles.searchItem}>
+            <View>
+              <Text style={styles.friendNameText}>{item.name}</Text>
+              <Text style={styles.friendEmailText}>{item.email}</Text>
+            </View>
+            {
+              this.state.friends.some(e => e.email === item.email)?
+                <Ionicons style={styles.removeButton} name="person-remove" size={28} onPress={() => this.removeFriendOnPress(item)} />:
+                <Ionicons style={styles.removeButton} name="person-add" size={28} onPress={() => this.addFriendOnPress(item)} />
+            }
+          </View>,
+          style: styles.list,
+        }}
+        listContainerStyle={styles.listContainer}
+      />
       <FlatList
         data={this.state.friends}
         renderItem={({ item }) => <this.ItemRender friend={item} />}
@@ -82,6 +185,16 @@ const styles = StyleSheet.create({
     color: "gray",
     fontSize: 16,
     fontWeight: "300",
+  },
+  autocomplete: {
+    marginHorizontal: 20,
+    marginTop: 10,
+    borderRadius: 10,
+    padding: 5,
+    backgroundColor: "white",
+  },
+  container: {
+    width: "100%",
   },
   divider: {
     alignSelf: "center",
@@ -105,8 +218,24 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     paddingVertical: 10,
   },
+  list: {
+    borderRadius: 10,
+    padding: 5,
+    backgroundColor: "white",
+  },
+  listContainer: {
+    marginHorizontal: 20,
+    backgroundColor: "white",
+    borderRadius: 10,
+  },
   removeButton: {
     alignSelf: "center",
     color: "#305DBF",
-  }
+  },
+  searchItem: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    margin: 5,
+  },
 });
